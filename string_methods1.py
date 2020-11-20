efb84eefa17b4934e4d643212d425787f8062f18
junaidmajeed <35539538+junaidmajeed@users.noreply.github.com>
2020-11-20 12:13:47 +0500

    Add files via upload

---
+++ string_methods1.py
@@ -0,0 +1,16 @@
+"""
+Examples to show available string methods in python
+"""
+
+# Replace Method
+a="1abc2abc3abc4abc"
+print(a.replace('abc','ABC',2))
+
+print(10*'*')
+
+# Sub-Strings
+sub = a[1:6]
+step = a[1:6:3]
+
+print(sub)
+print(step)
